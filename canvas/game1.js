2bac02d1d21d00fa905d2a04b9508ac187ae2953
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-09-06 19:30:42 -0400

    Update game1.js

--- canvas/game1.js
+++ canvas/game1.js
@@ -57,7 +57,7 @@ var myGameArea = {
   canvas: document.createElement('canvas'),
   start: function () {
     this.canvas.width = 650;
-    this.canvas.height = 500;
+    this.canvas.height = 400;
     this.context = this.canvas.getContext('2d');
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
