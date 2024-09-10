06987500de4922040bf8557c4c8da487c40dd486
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-09-10 15:32:09 -0400

    Update game1.js

--- canvas/game1.js
+++ canvas/game1.js
@@ -297,6 +297,9 @@ function displayGameOver() {
   ctx.font = "48px Arial";
   ctx.fillStyle = "red";
   ctx.textAlign = "center";
+  /// Trying 
+  ctx.clearRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height); // Clear previous text before showing pause
+/////
   ctx.fillText("Game Over!", myGameArea.canvas.width / 2, myGameArea.canvas.height / 2);
 }
 
