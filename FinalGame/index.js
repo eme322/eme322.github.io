b07afb1820d740534648a87694eae164d20ceaae
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-11-06 11:40:46 -0500

    Update index.js

--- FinalGame/index.js
+++ FinalGame/index.js
@@ -3,22 +3,32 @@ const c=canvas.getContext('2d');
 
 canvas.width = 1024
 canvas.height = 576
+
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2
 class Sprite {
   constructor({position, velocity}) 
   {
    this.position = position
     this.velocity = velocity
+    this.height = 150
   }
   draw(){
     c.fillStyle='red'
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
   }
   update(){
     this.draw()
-    this.position.y += 10
+    this.position.x += this.velocity.x 
+    this.position.y += this velocity.y
+    
+    if (this.position.y + this.height + this.velocity.y >= canvas.height){
+      
+      this.velocity.y = 0
+    } else this.velocity.y += gravity
   }
-  }
+}
 const player= new Sprite({
   position:{
   x:0,
@@ -41,11 +51,57 @@ const enemy= new Sprite({
 })
 
 console.log(player)
+
+const keys = {
+  a: { 
+    pressed: false
+  },
+  d: { 
+    pressed: false
+  }
+}
+let lastKey
+
+           
 function animate(){
   window.requestAnimationFrame(animate)
   c.fillstyle='black'
   c.fillRect(0,0, canvas.width, canvas.height)
  player.update()
-  enemy.update()
+  enemy.update
+
+  player.velocity.x = 0
+  
+  if (keys.a.pressed && lastKey === 'a') {
+    player.velocity.x = -1
+  } else if(keys.d.pressed && lastKey === 'd') {
+    player.veolocity.x = 1
+  }
 }
 animate()
+
+window.addEventListener('keydown', (event) => {
+  swith (event.key){
+    case 'd':
+    keys.d.pressed = true
+    lastKey = 'd'
+    break
+    case 'a':
+    keys.a.pressed = true
+    lastkey = 'a'
+    break
+  }
+  console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) => {
+  swith (event.key){
+    case 'd':
+    keys.d.pressed = false
+    break
+    case 'a':
+    keys.a.pressed = false
+    break
+  }
+  console.log(event.key)
+})
