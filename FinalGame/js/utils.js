eb33bab7cc7a41701dec0fa38ea912ea92f96a8b
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-12-12 11:58:08 -0500

    Update utils.js

--- FinalGame/js/utils.js
+++ FinalGame/js/utils.js
@@ -21,18 +21,3 @@ function determineWinner({ player1, player2, timerId }) {
     document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
   }
 }
-/*
-let timer = 60
-let timerId
-function decreaseTimer() {
-  if (timer > 0) {
-    timerId = setTimeout(decreaseTimer, 1000)
-    timer--
-    document.querySelector('#timer').innerHTML = timer
-  }
-
-  if (timer === 0) {
-    determineWinner({ player1, player2, timerId })
-  }
-}
-*/
